fea8137525cc5ff0a46b8043d83b2e98940dda9a
NguyenTheLoc-chanh <22a1001d0199@students.hou.edu.com>
2025-08-15 09:42:38 +0700

    vite-absolute-relative-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { Button } from '@mui/material'
+import ModeSelect from '~/components/ui/ModeSelect'
+
 
 function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }} color='text.primary'>
-          Material UI Vite.js example
-        </Typography>
-        <Button variant='contained' color='secondary'>
-          Contained
-        </Button>
+        <ModeSelect />
       </Box>
     </Container>
   )
